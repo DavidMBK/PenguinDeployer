ec6982a80dc033fac872e6a792c5de6c314903d4
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-05-22 16:16:49 +0200

    Added service configuration module

---
+++ src/services.py
@@ -0,0 +1,46 @@
+import subprocess
+
+from module import Module
+import re
+
+
+class Services(Module):
+    selected_file: str
+
+    def service_onoff(self, service: str, enable: bool):
+        #attiva o disattiva un servizio
+
+        if enable:
+            run = ["./service_onoff.sh"] + " enable " + service
+        else:
+            run = ["./service_onoff.sh"] + " disable " + service
+
+        subprocess.call(run)
+
+    def sys_read(self):
+        #funzione per leggere le impostazioni attuali del sistema
+        pass
+
+    def conf_export(self, filename):
+        #aggiornare nel futuro
+        pass
+
+    def conf_import(self, filename):
+        self.selected_file = filename
+        pass
+
+    def configure(self):
+
+        conf = open(self.selected_file)
+        services = conf.read()
+        split = re.split(':|\n', services)
+
+        i = 0
+        while i < len(split) / 2:
+            self.service_onoff(split[i],split[i+1])
+            i += 2
+
+
+s = Services()
+s.conf_import("configs/services.config")
+s.configure()

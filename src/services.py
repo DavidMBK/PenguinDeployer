ddd063eee9f2b98362aae6d561fea9e60a4fea92
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-05-22 17:46:46 +0200

    services code updated

--- src/services.py
+++ src/services.py
@@ -5,7 +5,8 @@ import re
 
 
 class Services(Module):
-    selected_file: str
+    to_enable: list[str]
+    to_disable: list[str]
 
     def service_onoff(self, service: str, enable: bool):
         #attiva o disattiva un servizio
@@ -21,29 +22,53 @@ class Services(Module):
         path = ["./systemctl.sh"]
         result = subprocess.run(path, capture_output=True, text=True)
 
-
         return result.stdout
 
-    def conf_export(self, configurations, filename):
-        #aggiornare nel futuro
-        pass
+    def conf_export(self, filename):
+
+        confexp = open(filename, 'a')
+
+        for pack in self.to_enable:
+            confexp.write("\n" + pack + ":" + "enable")
+
+        for pack in self.to_disable:
+            confexp.write("\n" + pack + ":" + "disable")
 
     def conf_import(self, filename):
-        self.selected_file = filename
-        pass
+
+        conf = open(filename)
+        services = conf.read()
+        ssplit = re.split(':|\n', services)
+
+        i = 0
+        while i < len(ssplit) / 2:
+            if ssplit[i + 1] == "enable":
+                self.to_enable.append(ssplit[i])
+            elif ssplit[i + 1] == "disable":
+                self.to_disable.append(ssplit[i])
+            else:
+                print("Errore nel file di configurazione")
+            i += 2
 
     def configure(self):
 
-        conf = open(self.selected_file)
-        services = conf.read()
-        split = re.split(':|\n', services)
+        for service in self.to_enable:
+            self.service_onoff(service, True)
 
-        i = 0
-        while i < len(split) / 2:
-            self.service_onoff(split[i],split[i+1])
-            i += 2
+        for service in self.to_disable:
+            self.service_onoff(service, False)
 
 
-s = Services()
-s.conf_import("configs/services.config")
-s.configure()
+""" Testing
+if __name__ == "__main__":
+    s = Servicess()
+
+    #testing import
+    s.conf_import("configs/services/testconfig.config")
+
+    #testing export
+    s.conf_export("configs/services/testconfigexp.config")
+
+    #testing configuration (install/uninstall)
+    s.configure()
+"""

910c99e3ba21ebcebc0f8b394f53c6efad92c948
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-05-23 16:07:30 +0200

    Updated quickconfig

--- src/quickconfig.py
+++ src/quickconfig.py
@@ -6,7 +6,7 @@ class QuickConfig():
     def importconfig(self):
         pass
 
-    def exportconfig(self):
+    def exportconfig(self,exportpath):
         #esporta le configurazioni
 
         #crea un archivio tar
@@ -14,10 +14,10 @@ class QuickConfig():
         here = os.getcwd()
         shutil.make_archive("export","tar",here + "/configs")
 
-
-'''
-
-q = QuickConfig
+        #mettilo nella cartella desirata
+        shutil.move(here + "/export.tar",here + "/configs")
 
 
-'''
+q = QuickConfig()
+
+q.exportconfig()

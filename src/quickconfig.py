6d5fb0e2121b1cd3b39960e7feb015777c915424
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-05-23 17:15:04 +0200

    updated quickconfigs

--- src/quickconfig.py
+++ src/quickconfig.py
@@ -4,8 +4,11 @@ import shutil
 
 class QuickConfig():
 
+    def __init__(self, modules):
+        self.modules = modules
+
     def importconfig(self, to_import):
-        shutil.unpack_archive(to_import,os.getcwd() + "/configs","tar",)
+        shutil.unpack_archive(to_import, os.getcwd() + "/configs", "tar", )
 
     def exportconfig(self, exportpath):
         #esporta le configurazioni
@@ -17,6 +20,17 @@ class QuickConfig():
 
         #mettilo nella cartella desirata
         shutil.move(here + "/export.tar", exportpath)
+
+    def apply_configs(self):
+        #applica i file di configurazione selezionati
+
+        #codice non finito, migliorare dopo
+        for module in self.modules:
+            module.conf_import()
+
+    #Nota: per l'aggiunta additiva di programmi alla lista di programmi, basterebbe aggiungere una funzione
+    #di import che non resetta
+
 
 '''
 q = QuickConfig()

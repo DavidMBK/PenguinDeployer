5bc1da63e07501fef17b32cfbf231f20d795443a
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-05-23 16:55:06 +0200

    finished quickconfig exp-imp

--- src/quickconfig.py
+++ src/quickconfig.py
@@ -5,7 +5,7 @@ import shutil
 class QuickConfig():
 
     def importconfig(self, to_import):
-        shutil.unpack_archive(to_import,os.getcwd(),"tar",)
+        shutil.unpack_archive(to_import,os.getcwd() + "/configs","tar",)
 
     def exportconfig(self, exportpath):
         #esporta le configurazioni
@@ -18,10 +18,11 @@ class QuickConfig():
         #mettilo nella cartella desirata
         shutil.move(here + "/export.tar", exportpath)
 
-
+'''
 q = QuickConfig()
 
 q.exportconfig("test")
 
 print("\n" + os.getcwd())
 q.importconfig(os.getcwd() + "/test/export.tar")
+'''

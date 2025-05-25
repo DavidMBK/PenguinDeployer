dc861752c36289345ea86699f90a7f495c4ee6fa
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-05-25 16:15:14 +0200

    Environment code update

--- src/environment.py
+++ src/environment.py
@@ -1,27 +1,40 @@
 import re
+import subprocess
+
 from module import Module
 
-# Shell predefinita
-# Prompt
-# Editor predefinito
-# Temi desktop (se disponibile)
 
 class Environment(Module):
 
-    def __init__(self,configfolder):
+    def __init__(self, nconfigfolder):
+        super().__init__(nconfigfolder)
         self.shell = ""
         self.editor = ""
         self.prompt = ""
         self.hostname = ""
         self.gconfigs = False
-        self.configfolder = configfolder
+        self.gconfigs_filename = ""
 
     def sys_read(self):
-        #aggiungere dopo
         pass
 
     def set_env_configs(self):
-        pass
+
+        runs = ["./src/scripts/change_shell.sh"] + [self.shell]
+        subprocess.call(runs)
+
+        rune = ["./src/scripts/change_editor.sh"] + [self.editor]
+        subprocess.call(rune)
+
+        runp = ["./src/scripts/change_prompt.sh"] + [self.prompt]
+        subprocess.call(runp)
+
+        runh = ["./src/scripts/change_hostname.sh"] + [self.hostname]
+        subprocess.call(runh)
+
+        if self.gconfigs:
+            grun = ["./src/scripts/expimp_gconfigs.sh"] + ["imp"] + [self.gconfigs_filename]
+            subprocess.call(grun)
 
     def conf_export(self, filename):
         confexp = open(self.configfolder + "/" + filename, 'w')
@@ -31,11 +44,11 @@ class Environment(Module):
         confexp.write("\n prompt:" + self.prompt)
         confexp.write("\n hostname:" + self.hostname)
         confexp.write("\n gconfigs:" + str(self.gconfigs))
+        confexp.write("\n gconfigs_filename:" + filename)
 
         if self.gconfigs:
-            run = ["./src/scripts/expimp_gconfigs.sh"] + ""
+            run = ["./src/scripts/expimp_gconfigs.sh"] + ["exp"] + [filename]
             subprocess.call(run)
-
 
     def conf_import(self, filename):
 
@@ -48,9 +61,10 @@ class Environment(Module):
         self.prompt = esplit[5]
         self.hostname = esplit[7]
         self.gconfigs = bool(esplit[9])
+        self.gconfigs_filename = esplit[11]
 
     def configure(self):
-        pass
+        self.set_env_configs()
 
 
 #shells cat /etc/shells to find them, then pick one and use chsh
@@ -60,7 +74,7 @@ class Environment(Module):
 #export gnome configs
 
 
-'''
+
 if __name__ == "__main__":
     e = Environment()
 
@@ -70,6 +84,6 @@ if __name__ == "__main__":
     #testing export
     e.conf_export("src/configs/services/testconfigexp.config")
 
-    #testing configuration (install/uninstall)
-    e.configure()
-'''
+    #testing configuration
+    #e.configure()
+
